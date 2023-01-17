01335db5656cf8a8ad4005840e2d31b882cb53d8
federudiero <federudiero@gmail.com>
2023-01-17 17:23:04 -0300

    desafio19

---
+++ backend-desafio19-main/db/mensajes.js
@@ -0,0 +1,16 @@
+import * as model from './models/model.js'
+
+class MensajesMongoDB {
+    constructor() {}
+
+    leer() {
+        return model.mensajes.find({})
+    }
+    
+    guardar(mensaje) {
+        const mensajeModel = new model.mensajes(mensaje);
+        return mensajeModel.save()
+    }
+}
+
+export default MensajesMongoDB
